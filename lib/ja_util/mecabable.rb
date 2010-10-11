c0ba121fc94b0124fa0214faad859cedec065153
tily <tily05@gmail.com>
2010-10-11 09:35:34 +0900

    Japanese::Mecabable spec and implementation.

--- lib/ja_util/mecabable.rb
+++ lib/ja_util/mecabable.rb
@@ -1,11 +1,30 @@
+require 'MeCab'
 
 module Japanese
 
     module Mecabable
 
         def words
-          # not implemented yet
-          # TODO: Mecab を使って形態素解析して Japanese な文字列の配列を返す
+            mecab_nodes.map do |node|
+                Japanese(node.surface) do |str|
+                    str.feature = node.feature
+                end
+            end
+        end
+
+        alias parse words
+
+    private
+
+        def mecab_nodes
+            nodes = []
+            tagger = MeCab::Tagger.new 
+            node = tagger.parseToNode(self)
+            while node
+                nodes.push(node) if !node.feature.match(%r{^(EOS|BOS/EOS),})
+                node = node.next
+            end
+            nodes
         end
 
     end
---
+++ spec/mecabable_spec.rb
@@ -0,0 +1,61 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Japanese::Mecabable do
+
+    before do
+        @str = '犬は水を恐れる'
+        @str.extend(Japanese::Mecabable)
+    end
+
+    describe 'parsing methods' do
+
+        it '#words return Japanese words' do
+            @words = @str.words
+        end
+
+        it '#parse return Japanese words' do
+            @words = @str.parse
+        end
+
+        after do
+            @words.should == ['犬', 'は', '水', 'を', '恐れる']
+            @words.each do |word|
+                word.japanese?.should be_true
+                word.featured?.should be_true
+            end
+        end
+
+    end
+
+    describe '#mecab_nodes' do
+
+        before do
+            @nodes = Array.new(5) { mock('DummyNode') }
+            @nodes.each_with_index do |node, i|
+              node.should_receive(:next).once.and_return(@nodes[i+1])
+            end
+            @tagger = mock('DummyTagger')
+            @tagger.should_receive(:parseToNode).once.and_return(@nodes.first)
+            MeCab::Tagger.should_receive(:new).once.and_return(@tagger)
+        end
+
+        it 'returns mecab nodes' do
+            @nodes.each_with_index do |node, i|
+              node.should_receive(:feature).once.and_return('dummy feature')
+            end
+            @str.send(:mecab_nodes).should == @nodes
+        end
+
+        it 'returns neither EOS nor BOS/EOS node' do
+            @nodes[1..3].each_with_index do |node, i|
+              node.should_receive(:feature).once.and_return('dummy feature')
+            end
+            @nodes[0].should_receive(:feature).once.and_return('BOS/EOS,')
+            @nodes[4].should_receive(:feature).once.and_return('EOS,')
+            @str.send(:mecab_nodes).should == @nodes[1..3]
+        end
+
+    end
+
+end
+
